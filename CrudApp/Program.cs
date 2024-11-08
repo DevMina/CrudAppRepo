bacc74ccc1e7b878a32df49d65ac1649c9c2baa8
Mina Abdo <Mina.Abdo@linkdev.com>
2024-11-08 20:08:04 +0200

    remove comments

--- CrudApp/Program.cs
+++ CrudApp/Program.cs
@@ -15,7 +15,6 @@ builder.Services.AddTransient<IEmployeeService, EmployeeService>();
 builder.Services.AddTransient<IRepository<Employee>, Repository<Employee>>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
@@ -27,7 +26,6 @@ builder.Services.AddCors(options =>
 });
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -36,7 +34,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseRouting();
 app.UseCors("AllowAll");
-//app.UseEndpoints(endpoints => endpoints.MapControllers());
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
